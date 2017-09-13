03939023189409107d202729e71cbaf4adf9e360
Yuna Lee <yunalee@usc.edu>
2017-09-13 01:12:33 -0700

    Added CIFAR-100 confusion matrix

--- runscript.sh
+++ runscript.sh
@@ -1,6 +1,6 @@
 for p in $(seq 0.0 0.2 1.0)
 do
-  th main.lua -dataset cifar100 -nGPU 2 -batchSize 128 -depth 20 -nEpochs 256 -LR 0.05 -experiment_type least -noisyLabelProbability $p
+  th main.lua -dataset cifar100 -nGPU 2 -batchSize 128 -depth 50 -nEpochs 256 -LR 0.05 -experiment_type random -noisyLabelProbability $p
   sleep 30s
 done
 exit 0
